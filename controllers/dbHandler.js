0040a269881ef98b6f39c694cf182c6e3ccf604d
monkmitrad <monkmitrad@gmail.com>
2021-01-31 18:14:46 +0100

    added dbHandler

---
+++ controllers/dbHandler.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+
+mongoose.connect(`mongodb://${config.get("db_host")}:${config.get("db_port")}/${config.get("db_name")}`, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+	console.log('Connection to MongoDB established');
+});
+
+module.exports = {
+
+};
